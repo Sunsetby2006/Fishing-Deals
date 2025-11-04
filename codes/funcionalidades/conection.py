48b8a05d84242835c375130509b95b0a4127c87f
Omar Guevara <169097622+Sunsetby2006@users.noreply.github.com>
2025-11-04 11:00:32 -0600

    Refactor connection code into get_connection function
    Added a function to establish a database connection with error handling.

--- codes/funcionalidades/conection.py
+++ codes/funcionalidades/conection.py
@@ -1,7 +1,13 @@
 #Hola a todos :DD
 import mysql.connector as dbc # requiere "pip install mysql-connector-python" en cmd
-conection = dbc.connect(user='root', password='Omarfgc113.', #Para pruebas cada quien debe poner su contraseña de MySQL
-                        host='127.0.0.1',
-                        database="datos_tienda")
-
-print("Se logro conectar a la DB, podemos seguir con el resto de actividades")
+def get_connection():
+    try:
+        connection = dbc.connect(
+            user='root', password='Omarfgc113.',  # Para pruebas cada quien debe poner su contraseña de MySQL
+            host='127.0.0.1', database="datos_tienda"
+        )
+        print("Se logró conectar a la DB, podemos seguir con el resto de actividades")
+        return connection
+    except Exception as e:
+        print(f"Error al conectar a la DB: {e}")
+        return None
